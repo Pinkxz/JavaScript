b4cd86bde316b8fe841bc4a94d416a466f060e8a
pinkxz <victor666157@gmail.com>
2025-03-23 15:43:42 -0300

    testando anon functions

---
+++ funcoes anonimas/teste.js
@@ -0,0 +1,8 @@
+const soma = (a, b) => a + b;
+const mul = (a, b) => a * b;
+const sub = (a, b) => a - b;
+const div = (a, b) => a / b;
+console.log(soma(3,4));
+console.log(mul(2,5));
+console.log(sub(3,2));
+console.log(div(10,2));
